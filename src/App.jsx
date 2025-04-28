a7e49eaa699b3e582383d78dc14d9a5e6f47e622
echeverriaalex <aleex.naahuel@outlook.com>
2025-04-27 21:16:38 -0300

    Aplico router

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
-//import './App.css'
-import { useSelector } from 'react-redux'
 import Footer from './components/Footer/Footer'
-import Hero from './components/Hero/Hero'
 import Layout from './components/Layout/Layout'
 import { Navbar } from './components/Navbar/Navbar'
+import Routes from './routes/Routes'
 
 function App(){
-
-  const state = useSelector((state) => state.pokemon)
-
-  {
-    /* links de api 
-    const UrlAllProducts = "https://dummyjson.com/products?limit=0";
-    const UrlAllCategories = "https://dummyjson.com/products/categories"; 
-    */
-  }
-
   return (
     <Layout>
-      { console.log(state.data) }
-      { state.isloading && <p>Loading...</p> }
-      { state.error && <p>{state.error}</p> }
       <Navbar />
-      <Hero />
-      { /* <Footer /> */ }
+      <Routes />
+      <Footer />
     </Layout>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { GlobalStyles } from './components/styles/GlobalStyles.js'
 import { Provider } from 'react-redux'
-import { store } from './redux/index.js'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <GlobalStyles />
-      <App />
+      <PersistGate persistor={persistor}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
