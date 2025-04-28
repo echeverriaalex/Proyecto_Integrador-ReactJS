0501dddf98bab707ebe6c261014b6045368d198b
echeverriaalex <aleex.naahuel@outlook.com>
2025-04-27 21:17:12 -0300

    aplico axios

---
+++ src/axios/axios-pokemons.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { API_URL_ALL, API_URL, POKEMON_URL } from "../utils/constants";
+
+// Obtengo todos los pokemons de la API 1302 en total
+export const getAllPokemons = async () => {
+    try{
+        const response = await axios.get(API_URL_ALL);
+        return response.data.results;
+    }
+    catch (error) {
+        console.error("Error En AXIOS fetching all pokemons -> ", error);
+        throw error;
+    }   
+}
+
+// Obtengo 20 pokemons de la API por pagina
+export const getPokemons = async () => {
+    try{
+        const response = await axios.get(API_URL);
+        return response.data.results;
+    }
+    catch (error) {
+        console.error("Error En AXIOS fetching all pokemons -> ", error);
+        throw error;
+    }   
+}
+
+export const getInfoPokemonByID = async (id) => {
+    try{
+        const response = await axios.get(`${POKEMON_URL}${id}`);
+        return response.data;
+    }
+    catch (error) {
+        console.error("Error En AXIOS fetching pokemons by ID -> ", error);
+        throw error;
+    }   
+}
+
+export const getInfoPokemonByName = async (name) => {
+    try{
+        const response = await axios.get(`${POKEMON_URL}${name}`);
+        return response.data;
+    }
+    catch (error) {
+        console.error("Error En AXIOS fetching pokemons by Name -> ", error);
+        throw error;
+    }   
+}
