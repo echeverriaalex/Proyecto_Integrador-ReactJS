3832c7649bc28fe6006ce11d916fadc651b685b2
echeverriaalex <aleex.naahuel@outlook.com>
2025-04-27 21:18:21 -0300

    creo constantes

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const API_URL_ALL = "https://pokeapi.co/api/v2/pokemon?limit=1400&offset=0"
+export const API_URL = "https://pokeapi.co/api/v2/pokemon/"
+export const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/" // Cambiar por el pokemon que quieras
