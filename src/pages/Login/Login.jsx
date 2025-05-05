0533cd461536bad036d310289746ad0973147eeb
echeverriaalex <aleex.naahuel@outlook.com>
2025-05-04 21:05:42 -0300

    avanzo con login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+import { FormStyled, HeaderStyled, LoginWrapper, RegisterWrapper } from "./LoginStyles"
+import charizard from "../../assets/images/charizard.png"
+
+
+const Login = () => {
+    return(
+        <LoginWrapper>
+            <HeaderStyled>
+                <h2>Login</h2>
+                <img src={charizard} alt="" />
+            </HeaderStyled>
+            
+            <FormStyled>
+                <input type="text" placeholder="Usuario" />
+                <input type="password" placeholder="Contraseña" />
+                <button type="submit">Iniciar Sesión</button>
+            </FormStyled>
+            
+            <RegisterWrapper>
+                <p>¿No tenés cuenta?</p>
+                <NavLink to="/register">
+                    
+                    <button type="button">Registrarse</button>
+                </NavLink>
+            </RegisterWrapper>
+
+        </LoginWrapper>
+    )
+}
+
+export default Login;
---
+++ src/pages/Login/LoginStyles.js
@@ -0,0 +1,84 @@
+import { Form } from "react-router-dom";
+import styled from "styled-components";
+
+export const LoginWrapper = styled.div`
+    background: #c59382;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    max-width: 500px;
+    padding: 20px;
+    min-height: 60vh;
+    h2{
+        font-size: 2rem;
+    }
+`
+
+export const HeaderStyled = styled.div`
+    display: flex;
+    width: 100%;
+    padding: 20px;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+    background:#c04666;
+    border-radius: 10px;
+    border: 2px solid black;
+
+    h2{
+        font-family: "Bruno Ace SC", sans-serif;
+        font-family: "Tagesschrift", system-ui;
+        color: white;
+        font-style: normal;
+    }
+
+    img{
+        height: 100px;
+    }
+`;
+
+export const FormStyled = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+
+
+    input{
+        width: 100%;
+        padding: 10px;
+        border-radius: 10px;
+        border: 2px solid black;
+        font-size: 1rem;
+    }
+
+    button{
+        width: 100%;
+        font-size: 1.2rem;
+        padding: 10px;
+        border-radius: 10px;
+        background: #d98321;
+        color: white;
+    }
+
+    p{
+        font-size: 1.2rem;
+        font-family: "Tagesschrift", system-ui;
+        color: white;
+        width: 100%;
+        text-align: center;
+    }
+`
+
+export const RegisterWrapper = styled(FormStyled)`
+    width: 100%;
+    background: lightcoral;
+    button{
+        background: #32c0d3;
+    }
+`;
