0c1b4c77bb1dc12077fba35bea37c11c76ba16d7
echeverriaalex <aleex.naahuel@outlook.com>
2025-05-04 21:07:34 -0300

    agrego contantes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const API_URL_ALL = "https://pokeapi.co/api/v2/pokemon?limit=1400&offset=0"
-export const API_URL = "https://pokeapi.co/api/v2/pokemon/"
+export const API_URL = "https://pokeapi.co/api/v2/pokemon"
 export const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/" // Cambiar por el pokemon que quieras
+export const POKEMONS_TYPES = "https://pokeapi.co/api/v2/type?limit=100"
+export const SHIPPING_COST = 500;
