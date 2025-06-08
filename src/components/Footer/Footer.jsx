3774c8b6b9dc88f635eb8f510dc68e0c2e1b5ae5
Alex Echeverria <aleex.naahuel@outlook.com>
2025-06-08 20:53:51 -0300

    mejoro el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,26 @@
-import { FooterStyled } from "./FooterStyles"
+import { DeveloperContainer, FooterStyled, Project, ProjectContainer } from "./FooterStyles"
 
 const Footer = () => {
     return(
         <FooterStyled>
-            <p>Footer</p>
-            <p>Todos los derechos reservados &copy; 2025</p>
-            <p>Developed by Alex Nahuel Echeverria</p>
+            <h2>Explora mis otros Proyectos</h2>
+            <ProjectContainer>
+                <Project>
+                    <img src="https://carstorepremium.vercel.app/assets/images/logo.webp"/>
+                    <p>Luxury vehículos de lujo</p>
+                </Project>
+                <Project>
+                    <img src="https://statics.redaccionmayo.com.ar/2023/01/crop/63c1c82aca69a__680x453.webp"/>
+                    <p>MEGA_BUY</p>
+                </Project>
+            </ProjectContainer>
+            
+            <a href="https://github.com/echeverriaalex">👉 Conocé más de mis proyectos 👈</a>
+            
+            <DeveloperContainer>
+                <p>Developed with ReactJS by <a href="https://alexnahuelecheverria.vercel.app/">Alex Nahuel Echeverria</a></p>
+                <p>Todos los derechos reservados &copy; 2025</p>
+            </DeveloperContainer>
         </FooterStyled>
     )
 }
--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -3,8 +3,92 @@ import styled from "styled-components";
 export const FooterStyled = styled.footer`
     display: flex;
     flex-direction: column;
+    align-items: center;
     gap: 20px;
     width: 100%;
     padding: 20px;
-    background: red;
-`
+    background: #a01c05;
+    
+    
+    h2{
+        color: white;
+        font-size: 1.5rem;
+        text-align: center;
+    }
+    
+    p{
+        color: white;
+        font-size: 1.4rem;
+        text-align: center;
+    }
+
+    a{
+        font-size: 1.5rem;
+        color: #fff;
+        font-family: "Bruno Ace", sans-serif;
+    }
+`;
+
+export const ProjectContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    gap: 20px;
+    max-width: 1200px;
+    padding: 10px;
+`;
+
+export const Project = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    max-width: 1200px;
+    padding: 10px;
+    background: #e9b46e;
+
+    display: flex;
+    width: 250px;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    
+    padding: 10px;
+    border-radius: 8px;
+
+    img{
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+    }
+`;
+
+export const DeveloperContainer = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+
+    p{
+        font-family: "Caveat", cursive;
+    }
+
+    a{
+        color: #fff;
+        font-weight: bold;
+        text-decoration: underline;
+        animation: fadeIn .8s ease-in-out infinite alternate;
+    }
+
+    @keyframes fadeIn {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+        
+`;
