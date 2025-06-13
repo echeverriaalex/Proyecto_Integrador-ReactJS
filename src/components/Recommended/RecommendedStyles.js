147605a68b03dbb24439be052358ceef389161e0
Alex Nahuel Echeverria <aleex.naahuel@outlook.com>
2025-06-13 18:33:44 +0000

    creo contenedor de recomendados

---
+++ src/components/Recommended/RecommendedStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const RecommendedContainerStyled = styled.div`
+    display: grid;
+    justify-content: center;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 300px));
+    gap: 15px;
+    row-gap: 40px;
+    width: 100%;
+    max-width: 1400px;
+    justify-content: center;
+    /*background: gray;*/
+    padding: 20px;
+`;
