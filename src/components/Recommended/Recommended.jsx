70c5a67e0ea5a54089857ffe0c57bb5c4cd61863
Alex Nahuel Echeverria <aleex.naahuel@outlook.com>
2025-06-13 18:34:39 +0000

    agrego contendor y arreglo importacion de cards

--- src/components/Recommended/Recommended.jsx
+++ src/components/Recommended/Recommended.jsx
@@ -1,5 +1,6 @@
-import Card from "../Products/Card/Card";
+import Card from "../Card/Card";
 import { useSelector } from "react-redux";
+import { RecommendedContainerStyled } from "./RecommendedStyles"
 //import { getRandomsPokemonsFromAPI } from "../../../utils/getRandomsPokemons/getRandomsPokemonsFromAPI"
 //import { getRandomsPokemonsFromAPI } from "../../../utils/getRandomsPokemons";
  
@@ -9,18 +10,20 @@ const Recommended = () => {
     const { recommended } = useSelector((state) => state.recommended)
     return(
         <>
-            {
-                recommended.map( (item) => {
-                    <Card
-                        key = {item.id}
-                        id = {item.id}
-                        name = {item.name}
-                        sprites = {item.sprites}
-                        weigth ={ item.weight }
-                        types = {item.types}
-                    />
-                })                
-            }
+            <RecommendedContainerStyled>
+                {
+                    recommended.map( (item) => {
+                        <Card
+                            key = {item.id}
+                            id = {item.id}
+                            name = {item.name}
+                            sprites = {item.sprites}
+                            weigth ={ item.weight }
+                            types = {item.types}
+                        />
+                    })                
+                }
+            </RecommendedContainerStyled>
         </>
     )
 }
