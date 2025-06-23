31d732613987a697e784e5382a53a99e8ecdc176
Alex Echeverria <aleex.naahuel@outlook.com>
2025-06-22 21:18:07 -0300

    seccion de recomendados

---
+++ src/components/CategoriesRecommended/CategoriesRecommended.jsx
@@ -0,0 +1,120 @@
+import { useEffect, useState, useRef } from "react";
+import { CardContainerStyled, CardsContainerStyled, CategoriesRecommendedContainerStyled, CategoryContainerStyled, ImgContainerStyled } from "./CategoriesRecommendedStyles";
+import { POKEMON_TYPE } from "../../utils/constants";
+import { getDataFromType, getInfoPokemonByURLFromApi } from "../../axios/axios-pokemons";
+import TypeLabelContainer from "../../components/Products/Card/Components/TypeLabelContainer/TypeLabelContainer";
+import typeIcons from "../../utils/setIcon";
+
+const CategoriesRecommended = () => {
+
+    const containerRef = useRef(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [scrollLeft, setScrollLeft] = useState(0);
+
+    const categoriesList = [ "fire", "water", "rock", "electric", "psychic", "dark"];
+    const [ dataList, setDataList ] = useState([]);
+
+    const fetchData = async () => {
+
+        const allCategoriesData = [];
+
+        for (let i = 0; i < categoriesList.length; i++) {
+
+            let URL_TYPE = `${POKEMON_TYPE}${categoriesList[i]}`;
+
+            try {
+                const response = await getDataFromType(URL_TYPE);
+                //console.log("Response from API:", response);
+
+                const dataCategory = await Promise.all(
+                    response.pokemon.map(async item => {
+                        //console.log("Pokemon Info:", item.pokemon);
+                        const pokemonInfo = await getInfoPokemonByURLFromApi(item.pokemon.url);
+                        //console.log("Pokemon Info:", pokemonInfo);                
+                        return { type: categoriesList[i], ...pokemonInfo };
+                    })
+                );
+
+                allCategoriesData.push({
+                    category: categoriesList[i],
+                    pokemons: dataCategory,
+                });
+
+                
+                
+                
+                //setDataList( ...dataList, { category: categoriesList[i], pokemon: dataCategory } )
+
+            } catch (error) {
+                console.error("Error fetching categories:", error);
+            }
+        }
+        setDataList(allCategoriesData);
+        //console.log(allCategoriesData);
+    };
+
+    const handleMouseDown = (e) => {
+        setIsDragging(true);
+        setStartX(e.pageX - containerRef.current.offsetLeft);
+        setScrollLeft(containerRef.current.scrollLeft);
+    };
+
+    const handleMouseLeave = () => {
+        setIsDragging(false);
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    };
+
+    const handleMouseMove = (e) => {
+        if (!isDragging) return;
+        e.preventDefault();
+        const x = e.pageX - containerRef.current.offsetLeft;
+        const walk = (x - startX) * 1.5; // velocidad de scroll
+        containerRef.current.scrollLeft = scrollLeft - walk;
+    };
+    
+    useEffect(() => {
+        fetchData();
+    }, []);
+    
+    return (
+        <CategoriesRecommendedContainerStyled>
+            {
+                dataList.map((item, index) => (
+                    <CategoryContainerStyled key={index}>
+                        <h3>
+                            Pokemons type {item.category.toUpperCase()}
+                            <img src={typeIcons[item.category]} alt={item.category} />
+                        </h3>
+                        <CardsContainerStyled
+                            ref={containerRef}
+                            onMouseDown={handleMouseDown}
+                            onMouseLeave={handleMouseLeave}
+                            onMouseUp={handleMouseUp}
+                            onMouseMove={handleMouseMove}
+                        >
+                        { 
+                            item.pokemons.map((pokemon, idx) => (
+                                <CardContainerStyled key={idx} type={pokemon.type}>
+                                    <ImgContainerStyled>
+                                        <img src={pokemon.sprites.other["dream_world"].front_default || pokemon.sprites.other["official-artwork"].front_default} />
+                                    </ImgContainerStyled>
+                                    <p>{pokemon.id}</p>
+                                    <p>{pokemon.name}</p>
+
+                                    <TypeLabelContainer types={pokemon.types} />
+                                </CardContainerStyled>
+                            ))
+                        }
+                        </CardsContainerStyled>
+                    </CategoryContainerStyled>
+                ))
+            }
+        </CategoriesRecommendedContainerStyled>
+    );
+};
+
+export default CategoriesRecommended;
---
+++ src/components/CategoriesRecommended/CategoriesRecommendedStyles.js
@@ -0,0 +1,126 @@
+import styled from "styled-components";
+import { getCardColorByCategory } from "../../utils/setColorBackground";
+
+export const CategoriesRecommendedContainerStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+    width: 90%;
+    /*
+    background-color: brown;
+    padding: 20px;
+    */
+
+    @media (max-width: 576px) {
+        width: 100%;
+    }
+`;
+
+export const CategoryContainerStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 10px;
+    width: 100%;
+    
+    /*background-color: green;*/
+
+    h3{
+        display: flex;
+        align-items: center;
+        gap: 20px;
+        color: white;
+        font-size: 1.5rem;
+        font-weight: 700;
+        font-family: monospace;
+
+        img{
+            width: 30px;
+            height: 30px;
+        }
+    }
+
+    @media (max-width: 576px) {
+        padding: 0px;
+
+        h3{
+            color: white;
+            font-size: 1rem;
+            font-weight: 700;
+            font-family: monospace;
+        }
+    }
+`;
+
+export const CardsContainerStyled = styled.div`
+    display: flex;
+    gap: 10px;
+    overflow: auto;
+    padding: 20px 0px;
+    scroll-behavior: smooth;
+    cursor: grab;
+    /*box-shadow: 0px 15px 15px 0px rgba(92, 92, 92, 0.93);*/
+
+    /*
+    &::-webkit-scrollbar {
+        display: none;
+    }
+    */
+
+    @media (max-width: 576px) {
+        scrollbar: none;
+        gap: 5px;
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    }
+`;
+
+export const CardContainerStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    padding: 10px 5px;
+    min-width: 270px;
+    width: 270px;
+    background: ${({ type }) => getCardColorByCategory(type)};
+    border: 1px solid black;
+    border-radius: 10px;
+    user-select: none;
+
+    p{
+        font-size: 1.2rem;
+        font-weight: 700;
+        text-transform: capitalize;
+        font-family: 'Courier New', Courier, monospace;
+    }
+
+    @media (max-width: 576px) {
+        gap: 5px;
+        padding: 5px;
+        min-width: 250px;
+        padding: 10px;
+    }
+`;
+
+export const ImgContainerStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 200px;
+    /*background-color: lightpink;*/
+
+    img{
+        max-width: 100%;
+        max-height: 100%;
+    }
+
+    @media (max-width: 576px) {
+        height: 130px;
+    }
+`;
