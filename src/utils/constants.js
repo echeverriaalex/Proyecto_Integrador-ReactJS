6a032f50c059a40b922c6dca2a1903dce9ca6736
Alex Echeverria <aleex.naahuel@outlook.com>
2025-06-22 21:16:56 -0300

    nueva constante

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,4 @@ export const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/" // Cambiar por e
 export const POKEMONS_TYPES = "https://pokeapi.co/api/v2/type?limit=30"
 export const POKEMONS_SPECIES = "https://pokeapi.co/api/v2/pokemon-species?limit=1000&offset=0"
 export const SHIPPING_COST = 500;
+export const POKEMON_TYPE = "https://pokeapi.co/api/v2/type/" // Ejemplo: https://pokeapi.co/api/v2/type/fire
