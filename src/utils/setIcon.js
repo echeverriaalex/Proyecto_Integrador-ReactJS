c00902860e56fcd7aaca61c4cd8ff8ab63fdb196
Alex Echeverria <aleex.naahuel@outlook.com>
2025-06-22 21:15:54 -0300

    aplico uso de iconos

---
+++ src/utils/setIcon.js
@@ -0,0 +1,47 @@
+import bug from "../assets/PokemonIcons/bug.png";
+import dark from "../assets/PokemonIcons/dark.png";
+import dragon from "../assets/PokemonIcons/dragon.png";
+import electric from "../assets/PokemonIcons/electric.png";
+import fairy from "../assets/PokemonIcons/fairy.png";
+import fighting from "../assets/PokemonIcons/fighting.png";
+import fire from "../assets/PokemonIcons/fire.png";
+import flying from "../assets/PokemonIcons/flying.png";
+import ghost from "../assets/PokemonIcons/ghost.png";
+import grass from "../assets/PokemonIcons/grass.png";
+import ground from "../assets/PokemonIcons/ground.png";
+import ice from "../assets/PokemonIcons/ice.png";
+import normal from "../assets/PokemonIcons/normal.webp";
+import poison from "../assets/PokemonIcons/poison.png";
+import psychic from "../assets/PokemonIcons/psychic.png";
+import rock from "../assets/PokemonIcons/rock.png";
+import steel from "../assets/PokemonIcons/steel.png";
+import water from "../assets/PokemonIcons/water.png";
+import unknown from "../assets/PokemonIcons/unknown.png";
+import shadow from "../assets/PokemonIcons/shadow.png";
+import stellar from "../assets/PokemonIcons/stellar.png";
+
+const typeIcons = {
+    bug,
+    dark,
+    dragon,
+    electric,
+    fairy,
+    fighting,
+    fire,
+    flying,
+    ghost,
+    grass,
+    ground,
+    ice,
+    normal,
+    poison,
+    psychic,
+    rock,
+    steel,
+    water,
+    unknown,
+    shadow,
+    stellar
+};
+
+export default typeIcons;
