c170fd4e2d55ad4c580f221b082090c21fe781c8
Alex Echeverria <aleex.naahuel@outlook.com>
2025-06-29 20:33:04 -0300

    agrego portada para cada categoria

---
+++ src/utils/setHeroCategory.js
@@ -0,0 +1,47 @@
+import electric from "../assets/Hero category/electric.jpg";
+import fire from "../assets/Hero category/fire 2.jpg";
+import psychic from "../assets/Hero category/psychic 2.jpg";
+import dark from "../assets/Hero category/dark 1.jpg";
+import water from "../assets/Hero category/water.png";
+import dragon from "../assets/Hero category/dragon.jpg";
+import ghost from "../assets/Hero category/ghost.jpg";
+import fighting from "../assets/Hero category/fighting1.webp";
+import fairy from "../assets/Hero category/fairy.avif";
+import grass from "../assets/Hero category/grass.jpg";
+import ice from "../assets/Hero category/ice.png";
+import ground from "../assets/Hero category/ground.jpg";
+import poison from "../assets/Hero category/poison1.jpg";
+import bug from "../assets/Hero category/bug.avif";
+import normal from "../assets/Hero category/normal1.webp";
+import steel from "../assets/Hero category/steel.jpg";
+import flying from "../assets/Hero category/flying.jpg";
+import rock from "../assets/Hero category/rock.webp";
+import unknown from "../assets/Hero category/unknown.webp";
+import stellar from "../assets/Hero category/stellar.png";
+import shadow from "../assets/Hero category/shadow.jpg";
+
+const HeroCategory = {
+    bug,
+    dark,
+    dragon,
+    fire,
+    electric,
+    water,
+    ghost,
+    psychic,
+    fighting,
+    fairy,
+    grass,
+    ice,
+    ground,
+    poison,
+    normal,
+    steel,
+    flying,
+    rock,
+    unknown,
+    stellar,
+    shadow,
+};
+
+export default HeroCategory;
