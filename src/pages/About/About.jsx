d4ca31752a3212faf0105ea2e1273569b2b630cf
Alex Echeverria <aleex.naahuel@outlook.com>
2025-08-09 02:09:20 -0300

    Creo la seccion de about y lo adapto a responsive

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,66 @@
+import { AboutWrapper, CardsContainerStyled, CardWrapperStyled, LinkButtonStyled, TextContainerStyled } from "./AboutStyles";
+
 const About = () => {
   return (
-    <div className="about">
-      <h1>About Us</h1>
-      <p>We are a team of passionate developers dedicated to building amazing web applications.</p>
-      <p>Our mission is to create user-friendly and efficient software solutions that make a difference.</p>
-    </div>
+    <AboutWrapper>
+      <CardsContainerStyled>
+        <CardWrapperStyled>
+          <img src="https://www.yourcentralvalley.com/wp-content/uploads/sites/54/2024/08/2024_Pokemon_World_Championships_Event_Photo_6.jpg?w=2560&h=1440&crop=1" alt="About Us" />
+          <TextContainerStyled>
+            <h2>We are PokéWorld!</h2>
+            <p>
+              We are a digital platform that sells exclusively Pokémon products, 
+              such as collectible cards, action figures, and some all original and 
+              certified. That's not all! We also organize the Pokémon World Championship, 
+              awarding wonderful prizes to the winning fans. Join our wonderful community!
+            </p>
+          </TextContainerStyled>
+        </CardWrapperStyled>
+        <CardWrapperStyled>
+          <img src="https://www.serebii.net/themeparks/universalstudiosjapan/nolimitparade.jpg" alt="PokePark" />
+          <TextContainerStyled>
+            <h2>Don't miss our events</h2>
+            <p>
+              We invite you to a unique, fun, and unforgettable
+              experience! Come try it and learn all the new features.
+            </p>
+          </TextContainerStyled>
+        </CardWrapperStyled>
+        <CardWrapperStyled>
+          <img src="https://st2.depositphotos.com/1309454/7392/v/950/depositphotos_73921481-stock-illustration-shopping-online-background.jpg" alt="Card Image" />
+          <TextContainerStyled>
+            <h2>Shopping</h2>
+            <p>
+              Buy as much as you want, without restrictions or limits.
+              Everything will be saved on your device so you can continue
+              shopping at any time. We work with all payment methods.
+            </p>
+          </TextContainerStyled>
+        </CardWrapperStyled>
+        <CardWrapperStyled>
+          <img src="https://w7.pngwing.com/pngs/19/911/png-transparent-computer-security-computer-icons-threat-security-miscellaneous-computer-network-computer-wallpaper.png" alt="Card Image" />
+          <TextContainerStyled>
+            <h2>Security</h2>
+            <p>
+              We use an end-to-end encryption system for all purchases and 
+              transactions you make on our platform, wherever you are.
+            </p>
+          </TextContainerStyled>
+        </CardWrapperStyled>
+        <CardWrapperStyled>
+          <img src="https://png.pngtree.com/thumb_back/fh260/background/20230717/pngtree-online-technical-support-with-3d-rendering-call-center-operator-assisting-image_3893834.jpg" alt="Card Image" />
+          <TextContainerStyled>
+            <h2>Support</h2>
+            <p>
+              Online and in person. We'll help you with purchases,
+              services, and everything else you need. Always here
+              for you.
+            </p>
+          </TextContainerStyled>
+        </CardWrapperStyled>
+      </CardsContainerStyled>
+      <LinkButtonStyled to="/">Go Start</LinkButtonStyled>
+    </AboutWrapper>
   );
 }
 
---
+++ src/pages/About/AboutStyles.js
@@ -0,0 +1,145 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const AboutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  padding: 2rem;
+  width: 100%;
+
+  @media (max-width: 1000px){
+    padding: 10px;
+  }
+
+  @media (max-width: 576px) {
+    padding: 10px;
+    gap: 15px;
+  }
+`;
+
+export const CardsContainerStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 30px;
+  width: 100%;
+
+  & > :nth-child(even) {
+    flex-direction: row-reverse;
+    border-radius: 160px 10px 190px 10px;
+
+    img{
+      border-radius: 70px 10px 190px 70px;
+    }
+  }
+  
+  @media (max-width: 576px){
+    flex-direction: column;
+    gap: 20px;
+
+    & > :nth-child(even) {
+      flex-direction: column;
+      border-radius: 10px;
+
+      img{
+        border-radius: 10px;
+      }
+    }
+  }
+`;
+
+export const CardWrapperStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+  padding: 5px;
+  width: 100%;
+  background-color: #000000ff;
+  border-radius: 10px 150px 30px 180px;
+
+  img{
+    width: 60%;
+    height: 500px;
+    border-radius: 10px 150px 30px 180px;
+  }
+
+  @media (max-width: 1000px){
+    border-radius: 10px 100px 30px 100px;
+    gap: 5px;
+    img{
+      width: 55%;
+      border-radius: 10px 100px 30px 100px;
+    }
+  }
+
+  @media (max-width: 576px){
+    flex-direction: column-reverse;
+    border-radius: 10px;
+
+    img{
+      border-radius: 10px;
+      width: 100%;
+      height: auto;
+    }
+  }
+`;
+
+export const TextContainerStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 40%;
+  gap: 15px;
+  padding: 10px;
+
+  h2, p{
+    color: #fff;
+    width: 100%;
+  }
+
+  h2{
+    font-family: Montserrat, sans-serif;
+    font-size: 3.2rem;
+  }
+
+  p{
+    font-size: 1.3rem;
+    font-family: Barlow, sans-serif;
+  }
+
+  @media (max-width: 1000px){
+    width: 45%;
+
+    h2{
+      font-size: 2.5rem;
+    }
+
+    p{
+      font-size: 1.2rem;
+    }
+  }
+
+  @media (max-width: 576px){
+    width: 100%;
+    h2{
+      font-size: 2rem;
+    }
+    p{
+      font-size: 1rem;
+    }
+  }
+`;
+
+export const LinkButtonStyled = styled(NavLink)`
+  padding: 20px 50px;
+  background-color: #1a1a1aff;
+  background-color: #000000ff;
+  color: white;
+  border-radius: 10px;
+  text-decoration: none;
+  font-size: 1.4rem;
+  font-family: Montserrat, sans-serif;
+`;
