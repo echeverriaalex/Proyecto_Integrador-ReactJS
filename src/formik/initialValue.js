64dc0a542406ed746cb5375e2aeaa753f782b123
Alex Echeverria <aleex.naahuel@outlook.com>
2025-08-10 20:35:16 -0300

    agrego funciones para formik

---
+++ src/formik/initialValue.js
@@ -0,0 +1,11 @@
+export const checkoutInitialValues = {
+    name: "",
+    email: "",
+    password: ""
+};
+
+export const registerInitialValues = {
+    name: "",
+    email: "",
+    password: ""
+};
---
+++ src/formik/validationSchema.js
@@ -0,0 +1,25 @@
+import * as Yup from 'yup';
+
+/*
+export const registerValidationSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, 'Too Short!')
+        .max(100, 'Too Long!')
+        .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string()
+        .min(8, 'Password must be at least 8 characters')
+        .required('Required'),
+});
+*/
+
+export const registerValidationSchema = Yup.object({
+    name: Yup.string()
+        .min(2, 'Too Short!')
+        .max(100, 'Too Long!')
+        .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string()
+        .min(8, 'Password must be at least 8 characters')
+        .required('Required'),
+});
