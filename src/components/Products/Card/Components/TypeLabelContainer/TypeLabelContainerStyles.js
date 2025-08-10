c5a0b0b15b8b576d9657270c64f6ef6ad98a2e5b
Alex Echeverria <aleex.naahuel@outlook.com>
2025-08-10 20:25:13 -0300

    mejoras visuales y responsive

--- src/components/Products/Card/Components/TypeLabelContainer/TypeLabelContainerStyles.js
+++ src/components/Products/Card/Components/TypeLabelContainer/TypeLabelContainerStyles.js
@@ -5,19 +5,31 @@ export const CategoryContainerStyled = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    gap: 8px;
+    align-items: center;
+    gap: 5px;
+    
+    @media (max-width: 1000px) {
+        height: 90px;
+    }
+
+    @media (max-width: 576px) {
+        width: 100%;
+        
+    }
 `;
 
 export const CategoryStyled = styled.div`
     display: flex;
+    justify-content: center;
     align-items: center;
     gap: 8px;    
     padding: 10px 15px;
     border-radius: 10px;
     background: ${({ type }) => colorCategory[type]};
-    
+    height: 40px;
+
     p{
-        font-size: 1.1rem;
+        font-size: 1rem;
         font-family: "Times New Roman";
         font-weight: 600;
         text-transform: capitalize;
@@ -26,5 +38,9 @@ export const CategoryStyled = styled.div`
 
     img{
         height: 20px;
+    }
+
+    @media (max-width: 576px) {
+        width: 100%;
     }
 `;
