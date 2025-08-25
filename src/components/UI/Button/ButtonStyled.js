dd5e3337705e7488140c60a43d0d3bdeedc71661
Alex Echeverria <aleex.naahuel@outlook.com>
2025-08-25 00:27:50 -0300

    achico  tamaño

--- src/components/UI/Button/ButtonStyled.js
+++ src/components/UI/Button/ButtonStyled.js
@@ -1,15 +1,13 @@
 import styled from "styled-components";
 
-
 export const ButtonStyled = styled.button`
-    padding: 10px 20px;
+    padding: 10px 15px;
     border-radius: 10px;
     /*background: #d3852c;*/
     background: ${({ background }) => `${background || '#d3852c'}`};
     color: white;
     font-size: 18px;
     font-weight: 700;
-    width: 120px;    
     cursor: pointer;
     border: none;
 
